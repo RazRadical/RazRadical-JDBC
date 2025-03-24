92a480c7284ea80b99771fb48e594b98076867aa
RazaDon Willrich <razradical93@gmail.com>
2025-03-24 04:23:29 +0000

    Completed JDBC.

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -2,6 +2,7 @@
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -37,13 +38,41 @@ import util.ConnectionUtil;
 public class Lab {
 
     public void createSong(Song song)  {
-        //write jdbc code here
+
+        try (Connection connection = ConnectionUtil.getConnection();) {
+            
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO songs(id, title, artist) VALUES(?, ?, ?)");
+            preparedStatement.setInt(1, song.getId());
+            preparedStatement.setString(2, song.gettitle());
+            preparedStatement.setString(3, song.getArtist());
+
+            int rowsAffected = preparedStatement.executeUpdate();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        
     }
 
     public List<Song> getAllSongs(){
         List<Song> songs = new ArrayList<>();
 
-        //write jdbc code here
+        try {
+            
+            Connection connection = ConnectionUtil.getConnection();
+
+            String sql = "SELECT * FROM songs;";
+
+            Statement statement = connection.createStatement();
+
+            ResultSet rs = statement.executeQuery(sql);
+
+            while(rs.next()){
+                songs.add(new Song(rs.getInt(1),rs.getString(2), rs.getString(3)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         return songs;
     }
